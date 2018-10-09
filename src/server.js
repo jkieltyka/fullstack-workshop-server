f3b195077f9bdbe2ee90f5374c1b00c290e82861
James Baxley <james@apollographql.com>
2018-10-08 22:35:29 -0400

    enable operation registry

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const { ApolloServer } = require("apollo-server");
-const isEmail = require("isemail");
+const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
+const OperationRegistry = require('apollo-server-plugin-operation-registry')
+  .default;
 
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
-const MovieDataSource = require("./data-sources/movie");
-const LikesDataSource = require("./data-sources/likes");
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const MovieDataSource = require('./data-sources/movie');
+const LikesDataSource = require('./data-sources/likes');
 
 // Set up Apollo Server
 const server = new ApolloServer({
@@ -14,8 +16,8 @@ const server = new ApolloServer({
   resolvers,
   context: ({ req }) => {
     // simple auth check on every request
-    const auth = (req.headers && req.headers.authorization) || "";
-    const email = Buffer.from(auth, "base64").toString("ascii");
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
 
     return { user: isEmail.validate(email) ? email : null };
   },
@@ -27,6 +29,7 @@ const server = new ApolloServer({
   engine: process.env.ENGINE_API_KEY
     ? { apiKey: process.env.ENGINE_API_KEY }
     : false,
+  plugins: [new OperationRegistry()],
 });
 
 // Start our server
