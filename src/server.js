f54f0f53cefc64d14dcd6041e74d332e19bc39fb
Peggy Rayzis <peggyrayzis@gmail.com>
2018-12-05 03:24:13 -0800

    Remove Apollo Server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,6 @@
 require('dotenv').config();
 
-const { ApolloServer } = require('apollo-server');
 const isEmail = require('isemail');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
-// Set up Apollo Server
-const server = new ApolloServer({
-  typeDefs,
-  // resolvers,
-});
-
-// Start our server
-server.listen().then(({ url }) => console.log(`🚀 app running at ${url}`));
